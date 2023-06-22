7891161fc743d3c04c22ba5d30a29dfba6d2df73
Agnieszka Koń-Kogut <konagnieszka@gmail.com>
2023-06-22 21:29:54 +0200

    basic konva setup

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 const stage = new Konva.Stage({
   container: "container", // ID elementu canvas
-  width: 800,
-  height: 600,
+  width: 600,
+  height: 800,
 });
+
+const layer = new Konva.Layer();
+stage.add(layer);
+stage.draw();
+
+const uploadedFile = document.getElementById("photo-input");
+
+const uploadedImage = new Image();
+uploadedImage.onload = function () {
+  const image = new Konva.Image({
+    image: imageObj,
+    x: 0,
+    y: 0,
+    width: image.width,
+    height: image.height,
+  });
+
+  layer.add(image); //dodaj obraz do warstwy
+
+  stage.add(layer); //dodaj warstwę do sceny
+};
+
+uploadedImage.src = "";
